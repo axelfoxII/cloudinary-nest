b4582dc04a2514ea58af33f87385d82f44eece61
Alex Salvatierra <asfoxII@gmail.com>
2024-06-11 10:17:52 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ImagesModule } from './images/images.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ImagesModule,
+  ],
+})
+export class AppModule {}
